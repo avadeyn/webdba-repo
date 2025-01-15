e9233635f15bd1892d0dba80a0c4f797c70cf9e1
avadeyn <manadineaborja@iskolarngbayan.pup.edu.ph>
2025-01-15 20:29:14 +0800

    incomplete design for student portal

--- system-SIS/Program.cs
+++ system-SIS/Program.cs
@@ -36,9 +36,9 @@ app.MapControllerRoute(
     //pattern: "{controller=Account}/{action=Signin}/{id?}")
     //pattern: "{controller=Account}/{action=Signup}/{id?}")
     //PORTALS
-    pattern: "{controller=AdminPortal}/{action=Index}/{id?}")
+    //pattern: "{controller=AdminPortal}/{action=Index}/{id?}")
     //pattern: "{controller=FacultyPortal}/{action=Index}/{id?}")
-    //pattern: "{controller=StudentsPortal}/{action=Home}/{id?}")
+    pattern: "{controller=StudentsPortal}/{action=Home}/{id?}")
     //pattern: "{controller=AdmissionPortal}/{action=Index}/{id?}")
     .WithStaticAssets();
 
