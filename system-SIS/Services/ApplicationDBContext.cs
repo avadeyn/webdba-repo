6636ae3dabc4139703b6f2c7370a21c07fd6198a
avadeyn <manadineaborja@iskolarngbayan.pup.edu.ph>
2025-01-18 20:06:34 +0800

    change dbcontext
    dbcontext is changes to identitydbcontext for user roles

--- system-SIS/Services/ApplicationDBContext.cs
+++ system-SIS/Services/ApplicationDBContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using system_SIS.Models;
 
 namespace system_SIS.Services
 {
-	public class ApplicationDBContext : DbContext
+	public class ApplicationDBContext : IdentityDbContext
 	{
 		public ApplicationDBContext(DbContextOptions options) : base(options)
 		{
