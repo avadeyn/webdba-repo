b60e26b642c3622253fdd797bd4e32e04b375aaf
Althea <altheaamorjasis@iskolarngbayan.pup.edu.ph>
2025-01-18 21:01:45 +0800

    NEW

--- system-SIS/Services/ApplicationDBContext.cs
+++ system-SIS/Services/ApplicationDBContext.cs
@@ -11,7 +11,7 @@ namespace system_SIS.Services
 
 		}
 
-		// Students in white color is the name of the table in the database (dbSis)
-		public DbSet<Students> Students { get; set; }
+		// Students in white color is the name of the table in the database (dbSis)       
+		public DbSet<Students> Students { get; set; }     
 	}
 }
