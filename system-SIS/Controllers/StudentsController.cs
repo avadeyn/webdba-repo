8e9c282195496daedbbf77a7223386bc3c34ca8a
Althea <altheaamorjasis@iskolarngbayan.pup.edu.ph>
2025-01-23 12:10:38 +0800

    update

--- system-SIS/Controllers/StudentsController.cs
+++ system-SIS/Controllers/StudentsController.cs
@@ -1,113 +1,113 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using system_SIS.Models;
-using system_SIS.Services;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using system_SIS.Models;
+//using system_SIS.Services;
 
-namespace system_SIS.Controllers
-{
-	public class StudentsController : Controller
-	{
-		public StudentsController(ApplicationDBContext context, IWebHostEnvironment environment)
-		{
-			this.Context = context;
-		}
+//namespace system_SIS.Controllers
+//{
+//	public class StudentsController : Controller
+//	{
+//		public StudentsController(ApplicationDBContext context, IWebHostEnvironment environment)
+//		{
+//			this.Context = context;
+//		}
 
-		public ApplicationDBContext Context { get; }
+//		public ApplicationDBContext Context { get; }
 
-		public IActionResult Index()
-		{
-			// students in white color is the name of the table in the database (dbSis)
-			var students = Context.Students.ToList();
-			return View(students);
-		}
+//		public IActionResult Index()
+//		{
+//			// students in white color is the name of the table in the database (dbSis)
+//			var students = Context.Students.ToList();
+//			return View(students);
+//		}
 
-		public IActionResult Create()
-		{
-			return View();
-		}
+//		public IActionResult Create()
+//		{
+//			return View();
+//		}
 
-		[HttpPost]
-		public IActionResult Create(AddStudent addStudent)
-		{
+//		[HttpPost]
+//		public IActionResult Create(AddStudent addStudent)
+//		{
 
-			if (!ModelState.IsValid)
-			{
-				return View(addStudent);
-			}
+//			if (!ModelState.IsValid)
+//			{
+//				return View(addStudent);
+//			}
 
-			// save data to the database
-			Students student = new Students()
-			{
-				Name = addStudent.Name,
-				DateofBirth = addStudent.DateofBirth
-			};
+//			// save data to the database
+//			Students student = new Students()
+//			{
+//				Name = addStudent.Name,
+//				DateofBirth = addStudent.DateofBirth
+//			};
 
-			Context.Students.Add(student);
-			Context.SaveChanges();
+//			Context.Students.Add(student);
+//			Context.SaveChanges();
 
-			return RedirectToAction("Index", "Students");
-		}
+//			return RedirectToAction("Index", "Students");
+//		}
 
-		public IActionResult Edit(int id)
-		{
-			var student = Context.Students.Find(id);
+//		public IActionResult Edit(int id)
+//		{
+//			var student = Context.Students.Find(id);
 
-			if (student == null)
-			{
-				return RedirectToAction("Index", "Students");
-			}
+//			if (student == null)
+//			{
+//				return RedirectToAction("Index", "Students");
+//			}
 
-			var addStudent = new AddStudent()
-			{
-				Name = student.Name,
-				DateofBirth = student.DateofBirth
-			};
+//			var addStudent = new AddStudent()
+//			{
+//				Name = student.Name,
+//				DateofBirth = student.DateofBirth
+//			};
 
-			ViewData["StudentId"] = id;
+//			ViewData["StudentId"] = id;
 
-			return View(addStudent);
+//			return View(addStudent);
 
-		}
+//		}
 
-		[HttpPost]
-		public IActionResult Edit(int id, AddStudent addStudent)
-		{
-			var student = Context.Students.Find(id);
+//		[HttpPost]
+//		public IActionResult Edit(int id, AddStudent addStudent)
+//		{
+//			var student = Context.Students.Find(id);
 
-			if (student == null)
-			{
-				return RedirectToAction("Index", "Students");
+//			if (student == null)
+//			{
+//				return RedirectToAction("Index", "Students");
 
-			}
+//			}
 
-			if (!ModelState.IsValid)
-			{
-				ViewData["StudentId"] = id;
+//			if (!ModelState.IsValid)
+//			{
+//				ViewData["StudentId"] = id;
 
-				return View(addStudent);
-			}
+//				return View(addStudent);
+//			}
 
-			// update data to the database
-			student.Name = addStudent.Name;
-			student.DateofBirth = addStudent.DateofBirth;
+//			// update data to the database
+//			student.Name = addStudent.Name;
+//			student.DateofBirth = addStudent.DateofBirth;
 
-			Context.SaveChanges();
+//			Context.SaveChanges();
 
-			return RedirectToAction("Index", "Students");
-		}
+//			return RedirectToAction("Index", "Students");
+//		}
 
-		public IActionResult Delete(int id)
-		{
-			var student = Context.Students.Find(id);
-			if (student == null)
-			{
-				return RedirectToAction("Index", "Students");
-			}
+//		public IActionResult Delete(int id)
+//		{
+//			var student = Context.Students.Find(id);
+//			if (student == null)
+//			{
+//				return RedirectToAction("Index", "Students");
+//			}
 
-			Context.Students.Remove(student);
-			Context.SaveChanges(true);
+//			Context.Students.Remove(student);
+//			Context.SaveChanges(true);
 
-			return RedirectToAction("Index", "Students");
-		}
+//			return RedirectToAction("Index", "Students");
+//		}
 
-	}
-}
+//	}
+//}
--- system-SIS/Services/ApplicationDBContext.cs
+++ system-SIS/Services/ApplicationDBContext.cs
@@ -12,6 +12,6 @@ namespace system_SIS.Services
 		}
 
 		// Students in white color is the name of the table in the database (dbSis)
-		public DbSet<Students> Students { get; set; }
+		public DbSet<Admission_Form> Admission_Forms { get; set; }
 	}
 }
