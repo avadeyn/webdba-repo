3e8d557de067f37f1b96ffc8031ce89cc380c5d4
Althea <altheaamorjasis@iskolarngbayan.pup.edu.ph>
2025-01-23 13:08:24 +0800

    js

---
+++ system-SIS/wwwroot/js/Admin_Portal/Enrollment.js
@@ -0,0 +1,5 @@
+﻿// Function to select/deselect all checkboxes
+function selectAllCheckboxes(source) {
+    const checkboxes = document.querySelectorAll('.studentCheckbox');
+    checkboxes.forEach(checkbox => checkbox.checked = source.checked);
+}
---
+++ system-SIS/wwwroot/js/Admin_Portal/grade8.js
@@ -0,0 +1,16 @@
+﻿// Select the buttons by their IDs
+const editBtn = document.getElementById('editBtn');
+const deleteBtn = document.getElementById('deleteBtn');
+
+// Add event listeners to the buttons to stop event propagation
+if (editBtn) {
+    editBtn.addEventListener('click', function (event) {
+        event.stopPropagation(); // Prevent row click from being triggered
+    });
+}
+
+if (deleteBtn) {
+    deleteBtn.addEventListener('click', function (event) {
+        event.stopPropagation(); // Prevent row click from being triggered
+    });
+}
