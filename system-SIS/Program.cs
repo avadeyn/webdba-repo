5eac83feb21bcd2e1247a0c2e46c96adba3be58a
Althea <altheaamorjasis@iskolarngbayan.pup.edu.ph>
2025-01-24 19:54:40 +0800

    goods na

--- system-SIS/Program.cs
+++ system-SIS/Program.cs
@@ -1,164 +1,119 @@
-using system_SIS.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using system_SIS.Services;
 
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddDbContext<ApplicationDBContext>(options =>
+public class Program
 {
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    options.UseSqlServer(connectionString);
-});
+	public static async Task Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+		// Add services to the container.
+		builder.Services.AddControllersWithViews();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+
+		builder.Services.AddDbContext<ApplicationDBContext>(options =>
+		{
+			var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+			options.UseSqlServer(connectionString);
+		});
+
+		builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+		{
+			// Configure identity options here
+			options.Password.RequireDigit = true;
+			options.Password.RequireLowercase = true;
+			options.Password.RequireUppercase = true;
+			options.Password.RequireNonAlphanumeric = true;
+			options.Password.RequiredLength = 8;
+		})
+			.AddRoles<IdentityRole>()
+			.AddEntityFrameworkStores<ApplicationDBContext>();
+
+
+		var app = builder.Build();
+
+		// Configure the HTTP request pipeline.
+		if (!app.Environment.IsDevelopment())
+		{
+			app.UseExceptionHandler("/Home/Error");
+			// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+			app.UseHsts();
+		}
+
+		app.UseHttpsRedirection();
+		app.UseRouting();
+
+		app.UseAuthorization();
+
+		app.MapStaticAssets();
+
+		app.MapControllerRoute(
+			name: "default",
+			pattern: "{controller=Account}/{action=Signin}/{id?}")
+			.WithStaticAssets();
+
+
+
+		using (var scope = app.Services.CreateScope())
+		{
+			var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+			var roles = new string[] { "Admin", "Faculty", "Student", "Applicant" };
+
+			foreach (var role in roles)
+			{
+				if (!await roleManager.RoleExistsAsync(role))
+				{
+					await roleManager.CreateAsync(new IdentityRole(role));
+				}
+			}
+		}
+
+		using (var scope = app.Services.CreateScope())
+		{
+			var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+			string email = "nadinexschool@gmail.com";
+			string password = "@Admin123";
+
+			if (await userManager.FindByEmailAsync(email) == null)
+			{
+				var user = new IdentityUser();
+				user.Email = email;
+				user.UserName = email;
+
+				Console.WriteLine("Creating user: " + email);
+				await userManager.CreateAsync(user, password);
+
+				await userManager.AddToRoleAsync(user, "Admin");
+			}
+		}
+
+		using (var scope = app.Services.CreateScope())
+		{
+			var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+			string email = "altheaamorasis@gmail.com";
+			string password = "Faculty-01";
+
+			if (await userManager.FindByEmailAsync(email) == null)
+			{
+				var user = new IdentityUser();
+				user.Email = email;
+				user.UserName = email;
+
+				Console.WriteLine("Creating user: " + email);
+				await userManager.CreateAsync(user, password);
+
+				await userManager.AddToRoleAsync(user, "Faculty");
+			}
+		}
+
+
+		app.Run();
+	}
 }
-
-app.UseHttpsRedirection();
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-    name: "default",
-    //pattern: "{controller=Account}/{action=Signin}/{id?}")
-    //pattern: "{controller=Account}/{action=Signup}/{id?}")
-    //PORTALS
-    //pattern: "{controller=FacultyPortal}/{action=Index}/{id?}")
-    pattern: "{controller=AdminPortal}/{action=Index}/{id?}")
-    //pattern: "{controller=AdmissionPortal}/{action=Home}/{id?}")
-    //pattern: "{controller=StudentsPortal}/{action=Home}/{id?}")
-
-    .WithStaticAssets();
-
-app.Run();
-
-//using Microsoft.AspNetCore.Identity;
-//using Microsoft.EntityFrameworkCore;
-//using system_SIS.Services;
-
-
-//public class Program
-//{
-//	public static async Task Main(string[] args)
-//	{
-//		var builder = WebApplication.CreateBuilder(args);
-
-//		// Add services to the container.
-//		builder.Services.AddControllersWithViews();
-
-
-//		builder.Services.AddDbContext<ApplicationDBContext>(options =>
-//		{
-//			var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-//			options.UseSqlServer(connectionString);
-//		});
-
-//		builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
-//		{
-//			// Configure identity options here
-//			options.Password.RequireDigit = true;
-//			options.Password.RequireLowercase = true;
-//			options.Password.RequireUppercase = true;
-//			options.Password.RequireNonAlphanumeric = true;
-//			options.Password.RequiredLength = 8;
-//		})
-//			.AddRoles<IdentityRole>()
-//			.AddEntityFrameworkStores<ApplicationDBContext>();
-
-
-//		var app = builder.Build();
-
-//		// Configure the HTTP request pipeline.
-//		if (!app.Environment.IsDevelopment())
-//		{
-//			app.UseExceptionHandler("/Home/Error");
-//			// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//			app.UseHsts();
-//		}
-
-//		app.UseHttpsRedirection();
-//		app.UseRouting();
-
-//		app.UseAuthorization();
-
-//		app.MapStaticAssets();
-
-//		app.MapControllerRoute(
-//			name: "default",
-//			pattern: "{controller=Account}/{action=Signin}/{id?}")
-//			.WithStaticAssets();
-
-
-
-//		using (var scope = app.Services.CreateScope())
-//		{
-//			var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-
-//			var roles = new string[] { "Admin", "Faculty", "Student", "Applicant" };
-
-//			foreach (var role in roles)
-//			{
-//				if (!await roleManager.RoleExistsAsync(role))
-//				{
-//					await roleManager.CreateAsync(new IdentityRole(role));
-//				}
-//			}
-//		}
-
-//		using (var scope = app.Services.CreateScope())
-//		{
-//			var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
-
-//			string email = "nadinexschool@gmail.com";
-//			string password = "@Admin123";
-
-//			if (await userManager.FindByEmailAsync(email) == null)
-//			{
-//				var user = new IdentityUser();
-//				user.Email = email;
-//				user.UserName = email;
-
-//				Console.WriteLine("Creating user: " + email);
-//				await userManager.CreateAsync(user, password);
-
-//				await userManager.AddToRoleAsync(user, "Admin");
-//			}
-//		}
-
-//		using (var scope = app.Services.CreateScope())
-//		{
-//			var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
-
-//			string email = "altheaamorasis@gmail.com";
-//			string password = "Faculty-01";
-
-//			if (await userManager.FindByEmailAsync(email) == null)
-//			{
-//				var user = new IdentityUser();
-//				user.Email = email;
-//				user.UserName = email;
-
-//				Console.WriteLine("Creating user: " + email);
-//				await userManager.CreateAsync(user, password);
-
-//				await userManager.AddToRoleAsync(user, "Faculty");
-//			}
-//		}
-
-
-//		app.Run();
-//	}
-//}
 
 
